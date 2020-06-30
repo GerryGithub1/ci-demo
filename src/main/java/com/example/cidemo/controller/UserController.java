55837317758ff6dfd092c759d7f17192e07b89d2
Gerry <271314998@qq.com>
2020-06-30 20:58:29 +0800

    首次提交

---
+++ src/main/java/com/example/cidemo/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.example.cidemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 欢迎来到牧码人教育，做Java我们是专业的
+ *
+ * @创建人： 牧码人教育-Gerry
+ * @创建时间： 2020-6-30
+ * @功能描述：
+ */
+@RestController
+@RequestMapping("user")
+public class UserController {
+    @RequestMapping("add")
+    public String add() {
+        return "user added";
+    }
+}
